dfcb8fed0388cd42b8276300d21d421ceecc9aaa
kartbnb <44807628+kartbnb@users.noreply.github.com>
2020-04-21 14:31:29 +1000

    add activity route

--- application.py
+++ application.py
@@ -10,30 +10,23 @@ return_default = {'return_code': '200', 'return_info': 'success', 'result': Fals
 @app.route("/", methods=["GET"])
 def main_page():
     return_str = """
-    This is the server of ActiDiabet App. This server only provide APIs for ActiDiabet Application. \n
-    '/Intensity': get all intensity levels\n
+    This is the server of ActiDiabet App. This server only provide APIs for ActiDiabet Application. 
+    '/Intensity': get all intensity levels 
     """
 
     return return_str
-
-#@app.route("/activity",methods=["GET"])
-#def check():
-    # define a return dictionary
-#    return_dict = return_default.copy()
-
-#    if request.args is None:
-#        return_dict['return_code'] = '5004'
-#        return_dict['return_info'] = 'empty'
- #       return json.dumps(return_dict, ensure_ascii=False)
-#    return_str = db.get_activity()
- #   return_dict['result'] = return_str
-
-#    return json.dumps(return_dict, ensure_ascii=False)
 
 @app.route("/intensity", methods=["GET"])
 def get_intensity():
     return_dict = return_default.copy()
     return_str = db.get_intensity()
+    return_dict['result'] = return_str
+    return json.dumps(return_dict, ensure_ascii=False)
+
+@app.route("/activity", methods=["GET"])
+def get_activity():
+    return_dict = return_default.copy()
+    return_str = db.get_activity()
     return_dict['result'] = return_str
     return json.dumps(return_dict, ensure_ascii=False)
 
