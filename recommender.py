934eaf356bf1ab7f6edcf7c0ff313a2b36323698
irisw0219 <62684956+irisw0219@users.noreply.github.com>
2020-05-28 15:05:01 +1000

    update param
    from "consine" to "pearson"

--- recommender.py
+++ recommender.py
@@ -3,7 +3,7 @@ from surprise import KNNWithMeans
 
 # To use item-based cosine similarity
 sim_options = {
-    "name": "cosine",
+    "name": "pearson",
     "user_based": False,  # Compute  similarities between items
 }
 algo = KNNWithMeans(sim_options=sim_options)
